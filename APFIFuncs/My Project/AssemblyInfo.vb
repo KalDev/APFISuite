2bc1a98e8dfdfafc3c715df2760c19662632c2c0
KalDev <kalsius@gmail.com>
2014-02-03 21:21:41 +0000

    Added Rainbow

---
+++ APFIFuncs/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("APFICrypto")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("APFICrypto")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("86a16c8c-5afa-43d8-9c19-fac8bd644fc8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ APFIFuncs/Rainbow.vb
@@ -0,0 +1,40 @@
+﻿Imports APFI.Helper
+Imports System.IO
+
+Public Class Rainbow
+
+    Public Sub GenNHSNumberList(ByVal mHelper As Helper)
+        'Open the file for writing
+        Dim swWriter As New StreamWriter(mHelper.OutputFile)
+        Dim iOutput As Integer = 0
+        Dim StartTime As Date = Now
+        Dim EndTime As Date
+
+        'BIG LOOP
+        For i As Integer = 400000000 To 499999999
+            iOutput = mHelper.CheckNHSNumber(i)
+            If iOutput > -1 Then
+                swWriter.WriteLine(i & iOutput)
+            End If
+        Next
+
+        EndTime = Now()
+        Console.WriteLine("First Batch Written in {0} minutes", DateDiff(DateInterval.Minute, StartTime, EndTime))
+
+        'SECOND BIG LOOP
+        For i As Integer = 600000000 To 708800001
+            iOutput = mHelper.CheckNHSNumber(i)
+            If iOutput > -1 Then
+                swWriter.WriteLine(i & iOutput)
+            End If
+        Next
+
+        EndTime = Now()
+        Console.WriteLine("Finished in {0} minutes", DateDiff(DateInterval.Minute, StartTime, EndTime))
+
+        swWriter.Close()
+    End Sub
+
+
+
+End Class
